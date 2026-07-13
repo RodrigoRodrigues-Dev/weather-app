fa94551eb1ec3b23e4842337ef5a0e7b4853e366
Rodrigo Rodrigues <rodrigo.rsmail@gmail.com>
2026-07-13 19:32:42 -0300

    feat: integrate Vercel Analytics for visitor tracking

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { RouterView } from 'vue-router';
 import Header from './components/Header.vue';
+import { Analytics } from '@vercel/analytics/vue';
 </script>
 
 <template>
@@ -10,6 +11,7 @@ import Header from './components/Header.vue';
     <Header />
     <RouterView />
   </div>
+  <Analytics />
 </template>
 
 <style scoped>
